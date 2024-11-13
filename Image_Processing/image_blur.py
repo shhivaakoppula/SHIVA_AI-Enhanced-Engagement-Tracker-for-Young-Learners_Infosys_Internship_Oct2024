78556bba5196e8dd93dc53b45cfa63bd790b3149
Koppula Shiva Goud <sk.goudkoppula8367@gmail.com>
2024-11-13 16:04:44 +0530

    Delete Image_Processing directory

--- Image_Processing/image_blur.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("C:\\Users\\Shiva Goud\\Pictures\\Screenshots\\chikki.png")
-blur = cv2.GaussianBlur(img, (15, 15), 0)
-
-cv2.imshow('Blurred Image', blur)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
